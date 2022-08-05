2cee93a745376156d7ecb9db695b9b3f27db9214
Daniel <Daniel@COMPUTADOR>
2022-08-05 08:24:22 +0100

    Adicionar arquivos de projeto.

---
+++ CodingDojoProblems/CodingDojoProblems/CodingDojoProblems/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Diamond;
+
+public class Diamond
+{
+    private static readonly char[] alphabeth = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+    public static void Main()
+    {
+        int row = 0;
+        int spaceCount = 0;
+        for (int i = 0; i <alphabeth.Length; i++)
+        {
+            do
+            {
+                Console.Write(alphabeth[i]);
+
+                if(row != 0 && row < alphabeth.Length)
+                {
+                    spaceCount = 0;
+                    do
+                    {
+                        Console.Write(' ');
+                        spaceCount++;
+                    } while (spaceCount <= row);
+
+                    Console.Write(alphabeth[i]);
+                }
+                Console.WriteLine();
+            } while (i < row);
+            row++;            
+        }
+    }
+}
