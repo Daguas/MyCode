cb5d12bc307d4d64d8cd3f3630ecd60cd73e8aff
Daniel Águas <danieljorge.oa@gmail.com>
2022-08-07 22:14:09 +0100

    Kata_RomanNumerals : Clean Code

--- CodingDojoProblems/CodingDojoProblems/Kata_RomanNumerals/Program.cs
+++ CodingDojoProblems/CodingDojoProblems/Kata_RomanNumerals/Program.cs
@@ -12,65 +12,20 @@ public class RomanNumerals
         Console.WriteLine(romanValue);
     }
 
-    private static string ConvertToRoman(int value)
+    private static string ConvertToRoman(int num)
     {
+        int size = num.ToString().Length - 1;
+
         string result = "";
-
-        int size = value.ToString().Length - 1;
-
-        Parallel.For(i)
-
 
         for (int i = 0; i <= size; i++)
         {
-            int house = int.Parse(value.ToString()[i].ToString());
-
-        }
-    }
-    private static string CheckFives(int i, int value)
-    {
-        string result = "";
-
-        switch (i)
-        {
-            case 3:
-                for (int j = 0; j < value; j++)
-                {
-                    result += "M";
-                }
-                break;
-            case 2:
-                if (value >= 5 && value < 9)
-                {
-                    result += "D";
-
-                    for (int j = 0; j < value - 5; j++)
-                    {
-                        result += "C";
-                    }
-                }
-                break;
-            case 1:
-                if (value >= 5 && value < 9)
-                {
-                    result += "L";
-
-                    for (int j = 0; j < value - 5; j++)
-                    {
-                        result += "X";
-                    }
-                }
-                break;
-            case 0:
-                if (value >= 5 && value < 9)
-                {
-                    result += "V";
-                    for (int j = 0; j < value - 5; j++)
-                    {
-                        result += "I";
-                    }
-                }
-                break;
+            int house = int.Parse(num.ToString()[i].ToString());
+        
+            result += CheckNines(size - i, house);
+            result += CheckFives(size - i, house);
+            result += CheckFours(size - i, house);
+            result += CheckLowers(size - i, house);
         }
 
         return result;
@@ -79,91 +34,123 @@ public class RomanNumerals
     private static string CheckNines(int i, int value)
     {
         string result = "";
-
-        switch (i)
+        if (value == 9)
         {
-            case 2:
-                if (value == 9)
-                {
+            switch (i)
+            {
+                case 2:
                     result += "CM";
-                }
-                break;
-            case 1:
-                if (value == 9)
-                {
+                    break;
+                case 1:
                     result += "XC";
-                }
-                break;
-            case 0:
-                if (value == 9)
-                {
+                    break;
+                case 0:
                     result += "IX";
-                }
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
         }
+        return result;
+    }
 
+    private static string CheckFives(int i, int value)
+    {
+        string result = "";
+        if (value >= 5 && value < 9)
+        {
+            switch (i)
+            {
+                case 3:
+                    for (int j = 0; j < value; j++)
+                    {
+                        result += "M";
+                    }
+                    break;
+                case 2:
+                    result += "D";
+                    for (int j = 0; j < value - 5; j++)
+                    {
+                        result += "C";
+                    }
+                    break;
+                case 1:
+                    result += "L";
+                    for (int j = 0; j < value - 5; j++)
+                    {
+                        result += "X";
+                    }
+                    break;
+                case 0:
+                    result += "V";
+                    for (int j = 0; j < value - 5; j++)
+                    {
+                        result += "I";
+                    }
+                    break;
+            }
+        }
         return result;
     }
 
     private static string CheckFours(int i, int value)
     {
         string result = "";
-
-        switch (i)
+        if (value == 4)
         {
-            case 2:
-                if (value == 4)
-                {
+            switch (i)
+            {
+                case 2:
                     result += "CD";
-                }
-                break;
-            case 1:
-                if (value == 4)
-                {
+                    break;
+                case 1:
                     result += "XL";
-                }
-                break;
-            case 0:
-                if (value == 4)
-                {
+                    break;
+                case 0:
                     result += "IV";
-                }
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
         }
-
         return result;
     }
 
-    private static string CheckExtra(int i, int value)
+    private static string CheckLowers(int i, int value)
     {
         string result = "";
 
-        switch (i)
+        if (value < 4)
         {
-            case 2:
-                for (int j = 0; j < value; j++)
-                {
-                    result += "C";
-                }
-                break;
-            case 1:
-                for (int j = 0; j < value; j++)
-                {
-                    result += "X";
-                }
-                break;
-            case 0:
-                for (int j = 0; j < value; j++)
-                {
-                    result += "I";
-                }
-                break;
-            default:
-                break;
+            switch (i)
+            {
+                case 3:
+                    for (int j = 0; j < value; j++)
+                    {
+                        result += "M";
+                    }
+                    break;
+                case 2:
+                    for (int j = 0; j < value; j++)
+                    {
+                        result += "C";
+                    }
+                    break;
+                case 1:
+                    for (int j = 0; j < value; j++)
+                    {
+                        result += "X";
+                    }
+                    break;
+                case 0:
+                    for (int j = 0; j < value; j++)
+                    {
+                        result += "I";
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
 
         return result;
